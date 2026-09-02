7496f0281b0524f2a4c9b6393d7610e02309509f
Emanuele <ema.belli.1997@gmail.com>
2026-09-02 12:44:11 +0200

    add cumulative filter function

--- data/filter.py
+++ data/filter.py
@@ -1,4 +1,5 @@
 import csv
+from typing import Callable
 from data.read_data import Table
 
 Row = list[str]
@@ -78,3 +79,47 @@ def remove_alphabetical_range(table: Table, field: str, min_value: str, max_valu
     if removed_at_least_once:
         return Table(table.fields, new_rows)
     raise ValueError(f"There are no values between {min_value} and {max_value} in the column {field}")
+
+
+def filter(table: Table, filters: dict):
+    filter_list: list[tuple[dict | None, Callable]] = [
+        (filters.get("exact_value"), filter_by_exact_value),
+        (filters.get("numerical_range"), filter_by_numerical_range),
+        (filters.get("alphabetical_range"), filter_by_alphabetical_range),
+        (filters.get("exact_value_remove"), remove_exact_value),
+        (filters.get("numerical_range_remove"),remove_numerical_range),
+        (filters.get("alphabetical_range_remove"), remove_alphabetical_range)
+    ]
+    filtered_table: Table = table
+    for i, item in enumerate(filter_list):
+        if item[0] is not None:
+            if i%3 == 0:
+                for field in item[0]:
+                    value = item[0].get(field)
+                    filtered_table = item[1](filtered_table, field, value)
+            else:
+                for field in item[0]:
+                    min_max: dict = item[0][field]
+                    filtered_table = item[1](filtered_table, field, **min_max)
+    save_csv(filtered_table)
+    return filtered_table
+
+def save_csv(table: Table):
+    pass
+            
+
+    
+
+
+filters: dict = {
+    "exact_value": {
+        "price": "550000.0",
+        "bedrooms": "3.0"
+    },
+    "numerical_range": {
+        "yr_built": {
+            "min_value": 1950,
+            "max_value": float("+inf")
+        }
+    }
+}
